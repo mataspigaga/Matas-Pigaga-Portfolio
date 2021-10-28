812480be4cf2c3e5c08761974c65994b8dfafce2
Matas Pigaga <tech@matas.io>
2021-10-28 14:32:59 -0400

    added basic Hero

---
+++ matas-pigaga-portfolio/components/Hero.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <>
+      <Image src='/avatar.png' width='200' height='200' />
+      <h1>Matas Pigaga</h1>
+      <h2>Web Developer | Digital Marketer | Bartender | DJ</h2>
+    </>
+  );
+};
+
+export default Hero;
