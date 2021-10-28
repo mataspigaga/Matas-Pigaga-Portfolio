dcb4a51fb9f42e8babf516537e8e452a7fb43ae3
Matas Pigaga <tech@matas.io>
2021-10-28 14:17:53 -0400

    updated to create-next-app

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b20fe99ed2e1f6e4"; }
+/******/ 		__webpack_require__.h = function() { return "b2c44e4bb2824a17"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
